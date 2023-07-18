822dcb9caf975e03d6381dabbbeb9d3f18d6de3b
henri <henri@moosbauer.com>
2023-07-18 16:20:13 +0200

    use custom dropdown

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -6,18 +6,67 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 <template>
   <div>
-    <div class="row gx-4 gy-2">
-      <div class="col-12 col-lg-4 position-relative">
-        <select
-          name="domain"
-          id="domain"
-          class="select-element w-100 border-0 form-control bg-light text-start"
-          v-model="domain"
+    <div class="d-flex flex-column flex-lg-row gap-2 gap-lg-4">
+      <div
+        class="col-12 col-lg-4 position-relative"
+      >
+        <div
+          id="dropdown-title"
+          class="select-element w-100 position-relative border-0 form-control bg-light dropdown-toggle text-start"
+          @click="toggleDropdown"
         >
-          <option value="all">All Domains</option>
-          <option value="tourism">Tourism</option>
-          <option value="mobility">Mobility</option>
-        </select>
+          {{ friendlyDomainNames[domain] }}
+        </div>
+        <div
+          id="dropdown"
+          class="w-100 position-absolute z-1 mt-2 card shadow"
+          :class="`${showDropdown ? 'd-block' : 'd-none'}`"
+        >
+          <ul class="mb-0 p-2 list-unstyled">
+            <li>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  id="all-radio"
+                  type="radio"
+                  value="all"
+                  v-model="domain"
+                >
+                <label class="form-check-label" for="all-radio">
+                  {{ friendlyDomainNames.all }}
+                </label>
+              </div>
+            </li>
+            <li>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  id="tourism-radio"
+                  type="radio"
+                  value="tourism"
+                  v-model="domain"
+                >
+                <label class="form-check-label" for="tourism-radio">
+                  {{ friendlyDomainNames.tourism }}
+                </label>
+              </div>
+            </li>
+            <li>
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  id="mobility-radio"
+                  type="radio"
+                  value="mobility"
+                  v-model="domain"
+                >
+                <label class="form-check-label" for="mobility-radio">
+                  {{ friendlyDomainNames.mobility }}
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="col-12 col-lg-4">
         <input
@@ -38,6 +87,17 @@ const emit = defineEmits<{
   domainChange: [domain: string];
   searchTermChange: [searchTerm: string];
 }>()
+
+const showDropdown = ref(false);
+function toggleDropdown() {
+  showDropdown.value = !showDropdown.value;
+}
+
+const friendlyDomainNames: Record<string, string> = {
+  all: "All domains",
+  tourism: "Tourism",
+  mobility: "Mobility"
+}
 
 const domain = ref("all");
 watch(domain, (newValue) => emit("domainChange", newValue));
